134f0ff13988f1df6a0c534b41ebed71d2616e72
Eirikur <easyfrag@gmail.com>
2023-04-06 13:14:23 +0000

    some updates

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let searchInterval;
   let countdownTimeout;
-  let intervalId = 5; // Add a variable to hold the interval ID for Audio repeat
 
   searchForm.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -14,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const searchString = document.getElementById("searchString").value.toLowerCase();
 	const interval = document.getElementById("interval").value * 1000;
 	const title = document.querySelector("h1");
-	title.classList.add("shaking"); // Add the 'shaking' class when the script is active
+	title.classList.add("shaking");
 
 	result.textContent = "Starting search...";
 	stopButton.disabled = false;
@@ -24,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const performSearch = () => {
 	  searchCount++;
 	  result.textContent = `Search attempt #${searchCount}: Fetching content from URL...`;
-	
+
 	  fetch(url)
 		.then((response) => {
 		  if (response.ok) {
@@ -36,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		.then((html) => {
 		  const lowerCaseHtml = html.toLowerCase();
 		  const searchStringIndex = lowerCaseHtml.indexOf(searchString);
-	
+
 		  if (searchStringIndex >= 0) {
 			result.textContent = `Search attempt #${searchCount}: Found the search string! Stopping search.`;
 			clearInterval(searchInterval);
@@ -50,19 +49,19 @@ document.addEventListener('DOMContentLoaded', function() {
 		  result.textContent = `Search attempt #${searchCount}: Error - ${error}. Retrying in ${interval / 1000} seconds...`;
 		});
 	};
-	
+
 	const startCountdown = (interval) => {
 	  let remainingSeconds = interval / 1000;
-	
+
 	  const updateCountdown = () => {
 		result.textContent = `Search attempt #${searchCount}: Search string not found. Retrying in ${remainingSeconds} seconds...`;
 		remainingSeconds--;
-	
+
 		if (remainingSeconds >= 0) {
-			countdownTimeout = setTimeout(updateCountdown, 1000);
+		  countdownTimeout = setTimeout(updateCountdown, 1000);
 		}
 	  };
-	
+
 	  updateCountdown();
 	};
 
@@ -79,50 +78,23 @@ document.addEventListener('DOMContentLoaded', function() {
   
 	const sound = document.getElementById(selectedSound);
 	sound.currentTime = 0;
-  
-	// Add an "ended" event listener to the audio element
-	sound.addEventListener("ended", () => {
-	  console.log("Notification sound ended");
-	  playNotificationSound(); // Play the sound again when it ends
-	});
-  
 	sound.play();
   
-	// Set an interval to play the sound every 10 seconds
-	intervalId = setInterval(() => {
+	const playSoundAgain = () => {
 	  sound.currentTime = 0;
 	  sound.play();
-	}, 10000);
+	};
   
-	// Show a dialog box and clear the interval when the user clicks OK
-	showAlertAndClearInterval(intervalId, sound);
+	setInterval(playSoundAgain, 10000);
   }
 
-
-  
-  function showAlertAndClearInterval(intervalId, sound) {
-	const soundSelect = document.getElementById("notificationSound");
-	const selectedSound = soundSelect.value;
-  
-	const alertPromise = new Promise((resolve) => {
-	  alert("Search string found! Close this to stop the sound.");
-	  resolve();
-	});
-  
-	alertPromise.then(() => {
-	  clearInterval(intervalId);
-	  sound.pause();
-	  sound.currentTime = 0;
-	});
-  }
-
-
-  stopButton.addEventListener("click", () => {
-	clearInterval(searchInterval);
-	clearTimeout(countdownTimeout); // Clear the countdown timeout
-	result.textContent = "Search stopped.";
-	stopButton.disabled = true;
-	const title = document.querySelector("h1");
-	title.classList.remove("shaking"); // Remove the 'shaking' class when the script is stopped
-  });
-});
+	  
+		stopButton.addEventListener("click", () => {
+		  clearInterval(searchInterval);
+		  clearTimeout(countdownTimeout);
+		  result.textContent = "Search stopped.";
+		  stopButton.disabled = true;
+		  const title = document.querySelector("h1");
+		  title.classList.remove("shaking");
+		});
+	  });
